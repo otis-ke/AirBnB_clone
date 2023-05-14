fd9218f33f226308acc67b8b4b726da049c7474e
Dadson Mweya <117777977+Dadsonk@users.noreply.github.com>
2023-05-14 21:35:10 +0300

    Update console.py

--- console.py
+++ console.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ console """
 
-
 import cmd
 from datetime import datetime
 import models
@@ -13,6 +12,7 @@ from models.review import Review
 from models.state import State
 from models.user import User
 import shlex  # for splitting the line along spaces except in double quotes
+
 
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
